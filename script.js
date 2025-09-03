551481dea2b3575dab9f18360c33433903da8858
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-09-02 21:42:31 -0400

    fixed functionality

--- script.js
+++ script.js
@@ -3,18 +3,12 @@ function calculateAge() {
     const birthDate = new Date(document.getElementById("dateSelection").value);
     const today = new Date();
 
-    // How the years, months, and days gets calculated
+    // How the years, months, and days get calculated.
     let years = today.getFullYear() - birthDate.getFullYear();
     let months = today.getMonth() - birthDate.getMonth();
     let days = today.getDate() - birthDate.getDate();
 
-    if (months <= 0 || (months === 0 && days <= 0)) {
-        months--;
-        const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
-        days += prevMonth.getDate();
-    }
-
-    if (days <= 0) {
+    if (days < 0) {
         months--;
        const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
        days += prevMonth.getDate();
@@ -29,12 +23,12 @@ function calculateAge() {
     document.getElementById("result").innerHTML = "Please enter your DOB!!!";
     return;
     } else {
-    document.getElementById("result").innerHTML = `Your age is ${years} years, ${months} months, ${days} days`;
+    document.getElementById("result").innerHTML = `Your age is ${years} years, ${months} months, ${days} days.`;
     return;
     }
 }
 
-function reset() {
+function resetAge() {
     // Resets the current output
     document.getElementById("result").innerHTML = "";
     return;
